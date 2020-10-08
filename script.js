60f3b1e641b53b8002ea15a632c8817dd734bf71
lizbarnett0 <lizcroper@gmail.com>
2020-10-08 14:08:34 -0500

    Preliminary logic for askQuestion function

--- script.js
+++ script.js
@@ -83,17 +83,36 @@ const tenQuestionDatabase = [
 	}
 ];
 
+//Scoring Variables
+let score = 0;
+const hard = 20;
+const medium = 10;
+const easy = 5
+
+//Document elements selectors
 const username = document.querySelector('input[type=text]');
 const letsPlayButton = document.querySelector('#letsPlayButton');
-let score = 0;
+
+//Document element creators
+const questionNum = document.createElement('h1');
+const questionDifficulty = document.createElement('span');
+const questionPointValue = document.createElement('span');
+const questionCategory = document.createElement('div');
+const questionText =  document.createElement('div');
+const answerChoices = document.createElement('div')
+const scoreDisplay = document.createElement('div')
+
 
 
 letsPlayButton.addEventListener('click', (event) => {
 	event.preventDefault();
-	gameStart();
-
+	console.log(username)
+	if (username.value === '' || username.value === username.placeholder) {
+		return alert('You need to enter a username to continue')
+	} else {
+		gameStart();
+	}
 })
-
 
 function gameStart() {
 	const currentUsername = username.value;
@@ -102,18 +121,32 @@ function gameStart() {
 	console.log(score);
 	resetScreen();
 	askQuestion();
-	
 }
 
 function resetScreen() {
 	document.body.innerHTML = '';
-
 }
 
 function askQuestion() {
-
+	for (let i = 0; i < tenQuestionDatabase.length; i++) {
+		questionNum.innerText = `Question ${i}`;
+		questionDifficulty.innerText = tenQuestionDatabase[i].difficulty;
+		questionCategory.innerText = tenQuestionDatabase[i].category;
+		questionText.innerText =  tenQuestionDatabase[i].question
+		answerChoices.innerText = `${tenQuestionDatabase[i].correct_answer} , ${tenQuestionDatabase[i].incorrect_answers[0]} ,${tenQuestionDatabase[i].incorrect_answers[1]}, ${tenQuestionDatabase[i].incorrect_answers[2]} `
+		scoreDisplay.innerHTML = score
+		if (tenQuestionDatabase[i].difficulty = 'hard') {
+			questionPointValue.innerText = tostring(hard);
+		} else if (tenQuestionDatabase[i].difficulty = 'medium') {
+			questionPointValue.innerText = toString(medium);
+		} else if (tenQuestionDatabase[i].difficulty = 'easy') {
+			questionPointValue.innerText = toString(easy);
+		} else {
+			questionPointValue.innerText = '0';
+		}
+	}
 }
 
-function gradeAnswer(params) {
+function gradeAnswer() {
 	
 }
